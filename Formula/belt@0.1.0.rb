d03807a5cff3750537f2134da8f36556339f852e
Rollie Ma <rma@masonhub.co>
2021-05-19 01:18:37 -0700

    latest version formula

--- Formula/belt@0.1.0.rb
+++ Formula/belt@0.1.0.rb
@@ -1,6 +1,6 @@
 require File.expand_path("../../Abstract/abstract_belt", __FILE__)
 
-class BeltAT010 < AbstractBelt
+class BeltAt010 < AbstractBelt
   version "0.1.0"
   init
   url "https://github.com/waltzofpearls/belt/releases/download/v0.1.0/belt-0.1.0-x86_64-apple-darwin.tar.gz"
---
+++ Formula/belt@latest.rb
@@ -0,0 +1,6 @@
+require File.expand_path("../../Abstract/abstract_belt", __FILE__)
+
+class BeltAtLatest < AbstractBelt
+  version "latest"
+  init
+end

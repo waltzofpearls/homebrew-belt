b24b1bd736bed2db38346497878ab649f72ff184
Rollie Ma <rollie@rollie.dev>
2021-05-19 01:20:07 -0700

    latest version formula (#3)

--- Formula/belt@0.1.2.rb
+++ Formula/belt@0.1.2.rb
@@ -1,6 +1,6 @@
 require File.expand_path("../../Abstract/abstract_belt", __FILE__)
 
-class BeltAT010 < AbstractBelt
+class BeltAt012 < AbstractBelt
   version "0.1.2"
   init
   url "https://github.com/waltzofpearls/belt/releases/download/v0.1.2/belt-0.1.2-x86_64-apple-darwin.tar.gz"

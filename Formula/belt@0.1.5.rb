dcd608b5d15ea2ccb1cb0048e31ac234f4be51bc
Rollie Ma <rma@masonhub.co>
2021-06-27 23:11:19 -0700

    version 0.1.5

--- Formula/belt@0.1.5.rb
+++ Formula/belt@0.1.5.rb
@@ -1,6 +1,6 @@
 require File.expand_path("../../Abstract/abstract_belt", __FILE__)
 
-class Belt < AbstractBelt
+class BeltAT010 < AbstractBelt
   version "0.1.5"
   init
   url "https://github.com/waltzofpearls/belt/releases/download/v0.1.5/belt-0.1.5-x86_64-apple-darwin.tar.gz"

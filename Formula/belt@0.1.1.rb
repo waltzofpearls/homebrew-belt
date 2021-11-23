8efa3099f435c16932e21613a04ce06eeaac4f9d
Rollie Ma <rma@masonhub.co>
2021-11-22 23:44:08 -0800

    fix brew tap

--- Formula/belt@0.1.1.rb
+++ Formula/belt@0.1.1.rb
@@ -1,6 +1,6 @@
 require File.expand_path("../../Abstract/abstract_belt", __FILE__)
 
-class BeltAt011 < AbstractBelt
+class BeltAT011 < AbstractBelt
   version "0.1.1"
   init
   url "https://github.com/waltzofpearls/belt/releases/download/v0.1.1/belt-0.1.1-x86_64-apple-darwin.tar.gz"

395aae966ee99d858e3fcf891e20d607f159d428
Rollie Ma <rollie@rollie.dev>
2023-11-13 11:04:24 -0800

    version 0.2.1

--- Formula/belt@0.2.1.rb
+++ Formula/belt@0.2.1.rb
@@ -1,6 +1,6 @@
 require File.expand_path("../../Abstract/abstract_belt", __FILE__)
 
-class Belt < AbstractBelt
+class BeltAT021 < AbstractBelt
   version "0.2.1"
   init
   url "https://github.com/waltzofpearls/dateparser/releases/download/v0.2.1/belt-0.2.1-macos-x86_64.zip"
